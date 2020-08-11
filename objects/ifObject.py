39cd62b4d98e21f52e4cf125db176633160f1f5e
Aguy11 <67076284+aguy11@users.noreply.github.com>
2020-08-11 11:26:39 -0400

    Create ifObject.py

---
+++ objects/ifObject.py
@@ -0,0 +1,8 @@
+class IfObject(object):
+  def __init__(self):
+    self.exec_code = ""
+  def transpile(self, case, indents):
+    self.exec_code = self.exec_code + f"if {case}:\n"
+    for i in range(indents):
+      self.exec_code = "\t" + self.exec_code
+    return self.exec_code
