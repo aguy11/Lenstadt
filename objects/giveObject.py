b4a726642d4e87a857c16041a4f01ff33e992115
Aguy11 <67076284+aguy11@users.noreply.github.com>
2020-08-16 14:49:54 -0400

    Create giveObject.py

---
+++ objects/giveObject.py
@@ -0,0 +1,8 @@
+class GiveObject(object):
+  def __init__(self):
+    self.exec_code = ''
+  def transpile(self, value, indents):
+    self.exec_code = self.exec_code + f"return {value}\n"
+    for i in range(indents):
+      self.exec_code = "\t" + self.exec_code
+    return self.exec_code
