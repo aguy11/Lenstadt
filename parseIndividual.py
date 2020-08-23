fc14d7e1e98fd27ff2d1cf4c25abbe0b74a4546b
Aguy11 <67076284+aguy11@users.noreply.github.com>
2020-08-23 11:39:01 -0400

    Update parseIndividual.py

--- parseIndividual.py
+++ parseIndividual.py
@@ -3,12 +3,14 @@ import parser, lexer
 class IndividualParser(object):
   def __init__(self, path):
     self.path = path
+    self.extras = ['time', 'random', 're', 'math']
   def add_code(self):
     with open(self.path, "r") as ooo:
       oo = ooo.read()
     lexe = lexer.Lexer(oo)
     toki = lexe.tokenize()
     par = parser.Parser(toki)
-    cod = par.parse()
-    return cod
+    cod = par.parse_extra()
+    
+    return cod, par.funcs
 
